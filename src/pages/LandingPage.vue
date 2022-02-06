4887ca58de4c2c1cb915387d3b3874139a71ddb8
Dominik Schneider <ds1.prgm@gmail.com>
2022-02-06 17:08:56 +0100

    firebase hosting

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -4,13 +4,10 @@
   <section
     class="flex justify-center items-center flex-col h-screen w-screen px-6 sm:px-0"
   >
-    <h1 class="font-display text-3xl mb-10">ChatApp</h1>
-    <h3 class="text-xl font-main w-full sm:w-96 text-center mb-6">
-      Chat with your Friends or people all over the world!
-    </h3>
+    <h1 class="text-3xl mb-4 font-main">Realtime Chat app!</h1>
     <router-link
       to="/signup"
-      class="block py-3.5 w-full sm:w-96 text-center text-base font-medium font-main rounded-sm text-gray-50 bg-gradient"
+      class="block py-3.5 w-full sm:w-96 text-center text-base font-medium font-main rounded-lg text-gray-50 bg-gradient"
     >
       Sign up
     </router-link>
